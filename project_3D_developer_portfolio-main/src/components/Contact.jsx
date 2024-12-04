2c8915674a1f43b05ae15127e776f1264d0c5c06
unknown <isob83709@gmail.com>
2024-12-04 18:47:26 +0200

    first commit

--- project_3D_developer_portfolio-main/src/components/Contact.jsx
+++ project_3D_developer_portfolio-main/src/components/Contact.jsx
@@ -1,6 +1,5 @@
 import React, { useRef, useState } from "react";
 import { motion } from "framer-motion";
-import emailjs from "@emailjs/browser";
 
 import { styles } from "../styles";
 import { EarthCanvas } from "./canvas";
@@ -31,37 +30,17 @@ const Contact = () => {
     e.preventDefault();
     setLoading(true);
 
-    emailjs
-      .send(
-        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
-        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
-        {
-          from_name: form.name,
-          to_name: "Badr",
-          from_email: form.email,
-          to_email: "isob83709@gmail.com",
-          message: form.message,
-        },
-        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
-      )
-      .then(
-        () => {
-          setLoading(false);
-          alert("Thank you. I will get back to you as soon as possible.");
+    // Simulate form submission
+    setTimeout(() => {
+      setLoading(false);
+      alert("Thank you. Your message has been sent!");
 
-          setForm({
-            name: "",
-            email: "",
-            message: "",
-          });
-        },
-        (error) => {
-          setLoading(false);
-          console.error(error);
-
-          alert("Ahh, something went wrong. Please try again.");
-        }
-      );
+      setForm({
+        name: "",
+        email: "",
+        message: "",
+      });
+    }, 1000);
   };
 
   return (
